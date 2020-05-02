97018e11d0b5bdf46e6a277982c5142ca82b907e
Nutchawit Pimpakan <nutchawit.p@outlook.com>
2020-05-02 09:50:32 +0700

    convert frontend to Docker

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '01bsrs6pi4'
+const apiId = 'uaim550u48'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
 export const authConfig = {
